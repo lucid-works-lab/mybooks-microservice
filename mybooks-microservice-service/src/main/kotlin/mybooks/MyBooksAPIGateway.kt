542519867c450e6a5bc91bbca4e9a11c620bb338
michaellif <michael.lifshitz@gmail.com>
2019-05-29 13:08:17 -0400

    local SAM works!!!

--- mybooks-microservice-service/src/main/kotlin/mybooks/MyBooksAPIGateway.kt
+++ mybooks-microservice-service/src/main/kotlin/mybooks/MyBooksAPIGateway.kt
@@ -2,32 +2,70 @@ package mybooks
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.cloud.function.adapter.aws.SpringBootApiGatewayRequestHandler
-import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.Message
+import org.springframework.messaging.support.GenericMessage
+import org.springframework.web.client.RestTemplate
+import java.util.function.Function
 
-@Component
+@Configuration
+class MyBooksAPIGateway {
+
+
+    @Bean
+    fun addBookResource(mapper: ObjectMapper): (APIGatewayProxyRequestEvent) -> APIGatewayProxyResponseEvent {
+        return {
+            println("+++++++++++++++++addBook  $it")
+            APIGatewayProxyResponseEvent()
+        }
+    }
+
+    @Bean
+    fun loadBookResource(restTemplate: RestTemplate): (APIGatewayProxyRequestEvent) -> APIGatewayProxyResponseEvent {
+        return {
+            println("+++++++++++++++++loadBook $it")
+            APIGatewayProxyResponseEvent()
+        }
+    }
+
+    @Bean
+    fun getAllBooksResource(): java.util.function.Function<Message<Any>, Message<String>> {
+        return Function {
+            println("+++++++++++++++++getAllBooks")
+            GenericMessage("Hellowwwwww11111",
+                    mapOf("Content-type" to "text/plain",
+                            "statusCode" to 202))
+
+        }
+    }
+
+}
+
+
 class AddBookEventHandler : SpringBootApiGatewayRequestHandler(MyBooksApp::class.java) {
     override fun handleRequest(event: APIGatewayProxyRequestEvent?, context: Context?): Any {
         println("+++++++++++++++++AddBookEventHandler $event")
-        System.setProperty("function.name", MyBooksApp::addBookResource.name)
+        System.setProperty("function.name", MyBooksAPIGateway::addBookResource.name)
         return super.handleRequest(event, context)
     }
 }
 
-@Component
 class LoadBookEventHandler : SpringBootApiGatewayRequestHandler(MyBooksApp::class.java) {
     override fun handleRequest(event: APIGatewayProxyRequestEvent?, context: Context?): Any {
         println("+++++++++++++++++LoadBookEventHandler $event")
-        System.setProperty("function.name", MyBooksApp::loadBookResource.name)
+        System.setProperty("function.name", MyBooksAPIGateway::loadBookResource.name)
         return super.handleRequest(event, context)
     }
 }
 
-@Component
 class GetAllBooksEventHandler : SpringBootApiGatewayRequestHandler(MyBooksApp::class.java) {
     override fun handleRequest(event: APIGatewayProxyRequestEvent?, context: Context?): Any {
         println("+++++++++++++++++GetAllBooksEventHandler ${event?.body}")
-        System.setProperty("function.name", MyBooksApp::getAllBooksResource.name)
+        System.setProperty("function.name", MyBooksAPIGateway::getAllBooksResource.name)
         return super.handleRequest(event, context)
     }
 }
--- mybooks-microservice-service/src/main/kotlin/mybooks/MyBooksApp.kt
+++ mybooks-microservice-service/src/main/kotlin/mybooks/MyBooksApp.kt
@@ -1,47 +1,16 @@
 package mybooks
 
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.messaging.Message
-import org.springframework.messaging.support.GenericMessage
 import org.springframework.web.client.RestTemplate
 import java.time.YearMonth
 
 @SpringBootApplication
 class MyBooksApp {
-
-    @Bean
-    fun addBookResource(mapper: ObjectMapper): (APIGatewayProxyRequestEvent) -> APIGatewayProxyResponseEvent {
-        return {
-            println("+++++++++++++++++addBook  $it")
-            APIGatewayProxyResponseEvent()
-        }
-    }
-
-    @Bean
-    fun loadBookResource(restTemplate: RestTemplate): (APIGatewayProxyRequestEvent) -> APIGatewayProxyResponseEvent {
-        return {
-            println("+++++++++++++++++loadBook $it")
-            APIGatewayProxyResponseEvent()
-        }
-    }
-
-    @Bean
-    fun getAllBooksResource(): (Message<Any>) -> Message<String> {
-        return {
-            println("+++++++++++++++++getAllBooks")
-            GenericMessage("Hellowwwwww11111",
-                    mapOf("Content-type" to "text/plain",
-                            "statusCode" to 202))
-
-        }
-    }
 
     @Bean
     fun getMapper(): ObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule())
