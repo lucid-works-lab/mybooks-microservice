652f3b134496d9b6c806e3022dd42c48d2bcd79b
michaellif <michael.lifschitz@gmail.com>
2019-06-17 07:27:02 -0400

    async test

--- mybooks-microservice-service/src/test/kotlin/mybooks/MyBooksStreamTest.kt
+++ mybooks-microservice-service/src/test/kotlin/mybooks/MyBooksStreamTest.kt
@@ -1,7 +1,6 @@
 package mybooks
 
 import mybooks.eventbus.Event
-import mybooks.eventbus.data.EventData
 import mybooks.eventbus.data.v1.BookAdded
 import mybooks.eventbus.meta.v1.EventMeta
 import org.junit.Assert
@@ -12,12 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-import org.springframework.context.annotation.Bean
 import org.springframework.test.context.junit4.SpringRunner
 import java.time.Instant
 import java.time.YearMonth
 import java.util.*
-import java.util.function.Consumer
 import java.util.function.Supplier
 
 @Tag("unit")
@@ -40,7 +37,7 @@ class MyBooksStreamTest {
                 authors = listOf("Miguel de Cervantes"),
                 published = YearMonth.of(2005, 4))
         eventStream.publishEvent(Event(id = UUID.fromString("9aef1f7e-90b1-11e9-bc42-526af7764f64"), meta = EventMeta(timestamp = Instant.now()), data = bookAddedEventData))
-        val book =  Book(isbn = "0060934344",
+        val book = Book(isbn = "0060934344",
                 title = "Don Quixote",
                 authors = listOf("Miguel de Cervantes"),
                 published = YearMonth.of(2005, 4))
