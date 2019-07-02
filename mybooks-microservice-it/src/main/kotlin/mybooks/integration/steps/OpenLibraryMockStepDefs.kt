8a8c116026615d763a65b926b9420436911d0204
michaellif <michael.lifschitz@gmail.com>
2019-07-02 17:41:42 -0400

    don't start wiremock inside cucumber

--- mybooks-microservice-it/src/main/kotlin/mybooks/integration/steps/OpenLibraryMockStepDefs.kt
+++ mybooks-microservice-it/src/main/kotlin/mybooks/integration/steps/OpenLibraryMockStepDefs.kt
@@ -1,8 +1,6 @@
 package mybooks.integration.steps
 
-import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration.options
 import mybooks.mocks.OpenLibraryMock
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -13,15 +11,7 @@ class OpenLibraryMockStepDefs : StepDefs {
 
     init {
         When("^running WireMock on host (\\S+) and port (\\S+)$") { host: String, port: Int ->
-
             WireMock.configureFor(host, port)
-
-            try {
-                val wireMockServer = WireMockServer(options().port(port))
-                wireMockServer.start()
-            } catch (e:Exception) {
-                println("Wiremock is probably already running on host $host and port $port")
-            }
         }
         When("^stabbing OpenLibraryMock for stub (\\S+)$") { stubType: String ->
             OpenLibraryMock().mockFor(OpenLibraryMock.StubType.valueOf(stubType))
